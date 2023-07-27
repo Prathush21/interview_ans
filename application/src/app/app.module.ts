71f9bdc8a36e39b879db6ad02c8d281fe767b149
Prathu <prathushan99@gmail.com>
2023-07-27 13:14:01 +0530

    task 2 completed

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,18 +1,28 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+
+import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FileUploadModule,
+    ButtonModule,
+    InputTextModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ application/src/app/form/form.component.ts
@@ -0,0 +1,46 @@
+// form.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  userForm: FormGroup;
+  isImageValid: boolean = true;
+  isImageDirty: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      profileImage: ['']
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.userForm.valid && this.isImageValid) {
+      console.log('Form data:', this.userForm.value);
+      // You can perform your form submission logic here, like calling an API to save the data.
+    } else {
+      console.log('Form data is invalid.');
+    }
+  }
+
+  onFileUpload(event: any) {
+    const file = event.files[0];
+    this.isImageDirty = true;
+
+    if (file && file.size > 5000000) {
+      this.isImageValid = false;
+      this.userForm.patchValue({ profileImage: null });
+    } else {
+      this.isImageValid = true;
+      this.userForm.patchValue({ profileImage: file });
+    }
+  }
+}
